7c4da5af1f69a68b26812bc7345693ab3c94d7e6
vkatika1 <vkatika1@asu.edu>
2024-10-23 20:33:28 -0700

    Fix for logger middleware

--- loggerMiddleware.js
+++ loggerMiddleware.js
@@ -1,7 +1,9 @@
-export const loggerMiddleware = (req, res, next) => {
+const loggerMiddleware = (req, res, next) => {
     const currentDateTime = new Date()
     const ipAddress = req.headers['x-forwarded-for'] || req.connection.remoteAddress
 
     console.log(`[${currentDateTime}] - ${req.method} ${req.url} - IP: ${ipAddress}`)
     next()
 }
+
+module.exports = { loggerMiddleware }
