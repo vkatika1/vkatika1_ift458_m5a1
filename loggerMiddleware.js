09aa1ca83d9a359bff9a4382b53a54599d4f9892
vkatika1 <vkatika1@asu.edu>
2024-10-23 20:28:52 -0700

    Add Logger Middleware

---
+++ loggerMiddleware.js
@@ -0,0 +1,7 @@
+export const loggerMiddleware = (req, res, next) => {
+    const currentDateTime = new Date()
+    const ipAddress = req.headers['x-forwarded-for'] || req.connection.remoteAddress
+
+    console.log(`[${currentDateTime}] - ${req.method} ${req.url} - IP: ${ipAddress}`)
+    next()
+}
